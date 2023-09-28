c668fffc61bf59d74841299ab258b2b80fe2668c
dimitrit <dimitri@tastypies.co.uk>
2023-09-28 06:40:55 +0100

    Improve command line utilities

--- txt2scr.sh
+++ txt2scr.sh
@@ -2,4 +2,30 @@
 #
 # Convert text to screens file format
 #
-nawk '/^[ 0-9]+/{$1="";printf"%-64s",substr($0,length($1)+2,64)}' $1
+p=64
+while getopts 's:h' opt; do
+  case "$opt" in
+    s)
+      p="$OPTARG"
+      ;;
+
+    ?|h)
+      echo "Usage: $(basename $0) [-p screens]"
+      exit 1
+      ;;
+  esac
+done
+shift "$(($OPTIND -1))"
+
+nawk '	BEGIN{lines=0}
+	/^[ 0-9]+/ {
+		gsub(/[^ -~]/,"",$0);
+		gsub(/^[ ]+[0-9]+[ ]?/,"",$0);
+		printf"%-64s",substr($0,0,64); lines++}
+	END{
+		for (i=0; i<(pages+0)-lines/16; i++) {
+			for (j=0;j<16;j++) {
+				printf("%-64s"," ");
+			}
+		}
+	}' pages=${p} $1
